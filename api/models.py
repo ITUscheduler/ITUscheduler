ff5fb764d7a775777ed65e7d191878d57e06d71c
dorukgezici <dorukgezici@gmail.com>
2017-08-16 14:32:48 +0300

    Course model changes

--- api/models.py
+++ api/models.py
@@ -1,3 +1,4 @@
+from django.core.validators import validate_comma_separated_integer_list
 from django.db import models
 
 
@@ -10,16 +11,17 @@ class CourseCode(models.Model):
 
 
 class Course(models.Model):
+    n_classes = models.PositiveSmallIntegerField(default=1)
     course_code = models.ForeignKey(CourseCode)
     crn = models.PositiveIntegerField(unique=True, primary_key=True)
     code = models.CharField(max_length=3)
     title = models.CharField(max_length=100)
     instructor = models.CharField(max_length=200)
-    building = models.CharField(max_length=3)
-    day = models.CharField(max_length=20)
-    time_start = models.PositiveIntegerField()
-    time_finish = models.PositiveIntegerField()
-    room = models.CharField(max_length=20)
+    building = models.CharField(max_length=20)
+    day = models.CharField(max_length=50)
+    time_start = models.CharField(max_length=40, validators=[validate_comma_separated_integer_list])
+    time_finish = models.CharField(max_length=40, validators=[validate_comma_separated_integer_list])
+    room = models.CharField(max_length=50)
     capacity = models.PositiveSmallIntegerField()
     enrolled = models.PositiveSmallIntegerField(default=0)
     reservation = models.CharField(max_length=50)
--- api/views.py
+++ api/views.py
@@ -1,55 +1,70 @@
 import requests
 from bs4 import BeautifulSoup
 from django.contrib.auth.decorators import user_passes_test
+from django.db import transaction
 from django.http import JsonResponse, HttpResponseNotFound, HttpResponse
 from django.core import serializers
 from api.models import CourseCode, Course
+from scheduler.models import Schedule
 
 BASE_URL = "http://www.sis.itu.edu.tr/tr/ders_programlari/LSprogramlar/prg.php?fb="
 
 
 @user_passes_test(lambda u: u.is_superuser)
 def db_refresh(request):
-    CourseCode.objects.all().delete()
-    Course.objects.all().delete()
+    with transaction.atomic():
+        CourseCode.objects.all().delete()
+        Course.objects.all().delete()
+        Schedule.objects.all().delete()
 
-    r = requests.get(BASE_URL)
-    soup = BeautifulSoup(r.content, "html.parser")
-
-    for option in soup.find("select").find_all("option"):
-        if option.attrs["value"] != "":
-            CourseCode.objects.create(code=option.get_text()[:-1:])
-
-    for course_code in CourseCode.objects.all():
-        r = requests.get(BASE_URL + course_code.code)
+        r = requests.get(BASE_URL)
         soup = BeautifulSoup(r.content, "html.parser")
 
-        i = 5
-        while True:
-            try:
-                raw_course = soup.select_one("tr:nth-of-type({})".format(i))
+        for option in soup.find("select").find_all("option"):
+            if option.attrs["value"] != "":
+                CourseCode.objects.create(code=option.get_text()[:-1:])
+
+        for course_code in CourseCode.objects.all():
+            r = requests.get(BASE_URL + course_code.code)
+            soup = BeautifulSoup(r.content, "html.parser")
+
+            i = 5
+            while True:
                 try:
-                    data = [row.get_text() for row in raw_course.find_all("td")]
-                    n = len(data[4]) // 3
-                    for index in range(n):
-                        time = data[6][:-1:].split()[index].split("/")
-                        if "" in time:
-                            time = ["2500", "2500"]
-                        if time[index][0] == "0":
-                            time[index] = time[index][1::]
-                        prerequisites = str(data[12])
+                    raw_course = soup.select_one("tr:nth-of-type({})".format(i))
+                    try:
+                        data = [row.get_text() for row in raw_course.find_all("td")]
+                        n = len(data[4]) // 3
+                        buildings = ",".join([data[4][3*i:3*i+3:] for i in range(n)])
+                        days = ",".join(data[5].split())
+                        times_start = ""
+                        times_finish = ""
+                        for index in range(n):
+                            time = data[6][:-1:].split()[index].split("/")
+                            if "" in time:
+                                time = ["2500", "2500"]
+                            for i in range(2):
+                                if time[i][0] == "0":
+                                    time[i] = time[i][1::]
+                            times_start += time[0] + ","
+                            times_finish += time[1] + ","
+                        times_start = times_start[:-1:]
+                        times_finish = times_finish[:-1:]
+                        rooms = ",".join(data[7].split())
+                        prerequisites = data[12]
                         prerequisites.replace("veya", " veya")
                         Course.objects.create(
+                            n_classes=n,
                             course_code=course_code,
                             crn=int(data[0]),
                             code=data[1],
                             title=data[2],
                             instructor=data[3],
-                            building=data[4][3*index:3*index+3:],
-                            day=data[5].split()[index],
-                            time_start=int(time[0]),
-                            time_finish=int(time[1]),
-                            room=data[7].split()[index],
+                            building=buildings,
+                            day=days,
+                            time_start=times_start,
+                            time_finish=times_finish,
+                            room=rooms,
                             capacity=int(data[8]),
                             enrolled=int(data[9]),
                             reservation=data[10],
@@ -57,11 +72,11 @@ def db_refresh(request):
                             prerequisites=prerequisites,
                             class_restriction=data[13]
                         )
-                    i += 1
-                except AttributeError as error:
-                    i += 1
-            except IndexError as error:
-                break
+                        i += 1
+                    except AttributeError:
+                        i += 1
+                except IndexError:
+                    break
 
     return HttpResponse("<a href='/'><h1>Course Codes and Courses refreshed!</h1></a>")
 
@@ -70,6 +85,7 @@ def db_refresh(request):
 def db_flush(request):
     CourseCode.objects.all().delete()
     Course.objects.all().delete()
+    Schedule.objects.all().delete()
 
     return HttpResponse("<a href='/'><h1>Course Codes and Courses flushed!</h1></a>")
 
@@ -132,9 +148,9 @@ def get_courses(request):
                             class_restriction=data[13]
                         ))
                     i += 1
-                except AttributeError as error:
+                except AttributeError:
                     i += 1
-            except IndexError as error:
+            except IndexError:
                 break
 
     params = {"sort_keys": True, "indent": 4}
