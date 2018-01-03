aa884d861f51229081e65bb42d0dc480ab4c6979
mehmetaltuner <altunerism@gmail.com>
2018-01-03 23:29:04 +0300

    API list views updated

--- api/rest_api/urls.py
+++ api/rest_api/urls.py
@@ -1,10 +1,10 @@
-from .views import CourseListAPIView, CourseDetailAPIView, CourseCodeDetailAPIView
+from .views import CourseListAPIView, CourseDetailAPIView, CourseCodeListAPIView
 from django.urls import path
 
 app_name = 'course_api'
 
 urlpatterns = [
-    path('courses-list', CourseListAPIView.as_view(), name='list_api'),
+    path('courses/<slug:course_code>', CourseListAPIView.as_view(), name='list_api'),
     path('detail/<int:pk>', CourseDetailAPIView.as_view(), name='list_detail'),
-    path('course-codes-list', CourseCodeDetailAPIView.as_view(), name='course_code_list_api'),
+    path('course-codes', CourseCodeListAPIView.as_view(), name='course_code_list_api'),
 ]
