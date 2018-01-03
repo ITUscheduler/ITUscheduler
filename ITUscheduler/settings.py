9900fb9745180923845f1f216b9127d105eea1d6
dorukgezici <dorukgezici@gmail.com>
2018-01-03 20:24:59 +0300

    New domain :)

--- ITUscheduler/settings.py
+++ ITUscheduler/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'on+@th%h49ncw2+v%i*8cgz8)6_@koy1j1rd7cq@s@=8y6(6%9'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.dorukgezici.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.ituscheduler.com', '.dorukgezici.com']
 
 
 # Application definition
