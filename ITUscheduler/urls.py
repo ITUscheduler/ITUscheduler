5ebbb3496bce6a2a1ff3bdf261ac9e635c4f26f0
dorukgezici <dorukgezici@gmail.com>
2018-01-03 21:19:57 +0300

    Attribute Error fixed

--- ITUscheduler/urls.py
+++ ITUscheduler/urls.py
@@ -20,11 +20,9 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/login/$', auth_views.LoginView.as_view(), name='login'),
-    path('accounts/logout/$', auth_views.LogoutView.as_view(next_page='/'), name='logout'),
+    path('accounts/login/', auth_views.LoginView.as_view(), name='login'),
+    path('accounts/logout/', auth_views.LogoutView.as_view(next_page='/'), name='logout'),
     path('', include('scheduler.urls')),
     path('api/', include('api.urls')),
-
     path('rest_api/', include('api.rest_api.urls', namespace='rest_api')),
-
 ]
--- scheduler/views.py
+++ scheduler/views.py
@@ -52,8 +52,12 @@ class IndexView(generic.CreateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         user = self.request.user
-        if not user.my_schedule:
+        try:
+            if not user.my_schedule:
+                raise AttributeError
+        except AttributeError:
             return context
+
         class Hour:
             def __init__(self, time, time_start, time_finish, course=None):
                 self.time = time
