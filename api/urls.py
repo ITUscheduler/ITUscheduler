d92eac4baf9599d789c5bae7b5d3119eebb287fd
dorukgezici <dorukgezici@gmail.com>
2018-01-03 23:05:22 +0300

    Urls updated

--- api/urls.py
+++ api/urls.py
@@ -1,11 +1,11 @@
-from django.conf.urls import url
+from django.urls import path
 from api import views
 
 urlpatterns = [
-    url(r'^refresh/courses/$', views.RefreshCoursesView.as_view(), name="refresh_courses"),
-    url(r'^db/refresh/course_codes/$', views.db_refresh_course_codes, name="db_refresh_course_codes"),
-    url(r'^db/refresh/courses/$', views.db_refresh_courses, name="db_refresh_courses"),
-    url(r'^db/flush/$', views.db_flush, name="db_flush"),
-    url(r'^course_codes/$', views.get_course_codes, name="api_course_codes"),
-    url(r'^courses/$', views.get_courses, name="api_courses")
+    path('refresh/courses', views.RefreshCoursesView.as_view(), name="refresh_courses"),
+    path('db/refresh/course_codes', views.db_refresh_course_codes, name="db_refresh_course_codes"),
+    path('db/refresh/courses', views.db_refresh_courses, name="db_refresh_courses"),
+    path('db/flush', views.db_flush, name="db_flush"),
+    path('course_codes', views.get_course_codes, name="api_course_codes"),
+    path('courses', views.get_courses, name="api_courses")
 ]
