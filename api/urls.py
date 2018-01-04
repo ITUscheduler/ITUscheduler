490d583c52460308138f8de2d64b2520f69c5270
dorukgezici <dorukgezici@gmail.com>
2018-01-04 04:09:47 +0300

    API functions cleaned

--- api/urls.py
+++ api/urls.py
@@ -5,6 +5,5 @@ urlpatterns = [
     path('refresh/courses', views.RefreshCoursesView.as_view(), name="refresh_courses"),
     path('db/refresh/course_codes', views.db_refresh_course_codes, name="db_refresh_course_codes"),
     path('db/refresh/courses', views.db_refresh_courses, name="db_refresh_courses"),
-    path('db/flush', views.db_flush, name="db_flush"),
-    path('course_codes', views.get_course_codes, name="api_course_codes")
+    path('db/flush', views.db_flush, name="db_flush")
 ]
