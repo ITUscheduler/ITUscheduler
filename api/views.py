9e2bd4a1e1c5f20dfa06049906ed4ad109ec6358
mehmetaltuner <altunerism@gmail.com>
2018-01-07 02:28:28 +0300

    Handling prerequisite creation modified

--- api/views.py
+++ api/views.py
@@ -89,9 +89,9 @@ def db_refresh_courses(request):
                             days = data[5].split()
                             majors = data[11].split(", ")
                             prerequisites_objects = []
-                            if 'Yok/None' not in prerequisites and 'Diğer Şartlar' not in prerequisites:
+                            if 'Yok/None' not in prerequisites and 'Diğer Şartlar' not in prerequisites and "Özel" :
                                 for prerequisite in prerequisites.split(' veya '):
-                                    prerequisites_objects.append(Prerequisite.objects.create(code=prerequisite[:9], min_grade=prerequisite[-2:]))
+                                    prerequisites_objects.append(Prerequisite.objects.get_or_create(code=prerequisite[:9], min_grade=prerequisite[-2:]))
                             else:
                                 prerequisites_objects.append(Prerequisite.objects.get_or_create(code=None)[0])
 
