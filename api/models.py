7e751ed6d570193be621ffba27406c3ac5f6286c
dorukgezici <dorukgezici@gmail.com>
2018-01-10 04:20:22 +0300

    Course __str__ updated

--- api/models.py
+++ api/models.py
@@ -49,7 +49,7 @@ class Course(models.Model):
             return True
 
     def __str__(self):
-        lectures = '#' + str(self.crn) + " " + str(self.title) + "\t| "
+        lectures = '#' + str(self.crn) + " " + str(self.code) + " " + str(self.title) + "\t| "
         for lecture in self.lecture_set.all():
             lectures += "{} {} {} {} | ".format(lecture.building, lecture.day, *lecture.time_str_tuple())
         lectures += str(self.enrolled) + "/" + str(self.capacity) + " Capacity"
