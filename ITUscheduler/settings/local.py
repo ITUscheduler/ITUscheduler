1b624c88ec2421cbd20d8f07ee99af7645671ea9
dorukgezici <dorukgezici@gmail.com>
2018-01-11 14:38:55 +0300

    Admin link added for superadmin

--- ITUscheduler/settings/local.py
+++ ITUscheduler/settings/local.py
@@ -7,7 +7,6 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-
     }, 'postgres': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': POSTGRES_NAME,
