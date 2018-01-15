4604e7599f68da6d9f51dbd48a1972610be904ed
mehmetaltuner <altuner17@itu.edu.tr>
2018-01-15 22:58:41 +0300

    login 404 error fixed

--- ITUscheduler/urls.py
+++ ITUscheduler/urls.py
@@ -4,7 +4,7 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/login', auth_views.LoginView.as_view(), name='login'),
+    path('accounts/login/', auth_views.LoginView.as_view(), name='login'),
     path('accounts/logout', auth_views.LogoutView.as_view(next_page='/'), name='logout'),
     path('accounts/reset-password', auth_views.PasswordResetView.as_view(), name="password-reset"),
     path('accounts/reset-password-done', auth_views.PasswordResetDoneView.as_view(), name="password_reset_done"),
