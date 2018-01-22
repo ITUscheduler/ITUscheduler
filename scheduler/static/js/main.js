00753d7a1c7f5525b1e2f370e6c8eaeb053a372f
dorukgezici <dorukgezici@gmail.com>
2018-01-22 14:10:09 +0200

    iOS Web App link fix 2

--- scheduler/static/js/main.js
+++ scheduler/static/js/main.js
@@ -13,3 +13,16 @@ function getCookie(name) {
     }
     return cookieValue;
 }
+
+$(document).ready(function() {
+    if (("standalone" in window.navigator) && window.navigator.standalone) {
+        // For iOS Apps
+        $('a').on('click', function(e) {
+            e.preventDefault();
+            var new_location = $(this).attr('href');
+            if (new_location != undefined && new_location.substr(0, 1) != '#' && $(this).attr('data-method') == undefined) {
+                window.location = new_location;
+            }
+        });
+    }
+});
