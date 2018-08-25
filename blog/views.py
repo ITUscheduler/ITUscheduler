b09a8ff0ca8a7bad29a9fcee4788c3fff68ba9af
dorukgezici <dorukgezici@gmail.com>
2018-08-25 23:45:44 +0300

    Small UI changes

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,7 @@
 from django.views.generic import ListView
 from django.core.paginator import Paginator
 from .models import Post
-from api.models import Course, MajorCode
+from api.models import Course, MajorCode, Semester
 from scheduler.models import Schedule
 from scheduler.models import ExtendedUser
 
@@ -10,15 +10,15 @@ class PostListView(ListView):
     queryset = Post.objects.all()
     context_object_name = 'posts'
     paginate_by = 3
-    refresh_paginator = Paginator(MajorCode.objects.all(), 17)
+    refresh_paginator = Paginator(MajorCode.objects.all(), 30)
 
     def get_context_data(self, **kwargs):
         context = super(PostListView, self).get_context_data(**kwargs)
-        deleted_courses_count = Course.objects.inactive().count()
+        courses_count = Course.objects.filter(semester=Semester.objects.current()).count()
         total_schedule_count = Schedule.objects.all().count()
         user_count = ExtendedUser.objects.all().count()
 
-        context["deleted_courses_count"] = deleted_courses_count
+        context["courses_count"] = courses_count
         context["total_schedule_count"] = total_schedule_count
         context["user_count"] = user_count
 
