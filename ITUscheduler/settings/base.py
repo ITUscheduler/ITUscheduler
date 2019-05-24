b7f5c17c0596934c9d40fe5f357fe577debf2dfb
dorukgezici <dorukgezici@gmail.com>
2019-05-24 21:23:06 +0300

    EC2 Private IP checked

--- ITUscheduler/settings/base.py
+++ ITUscheduler/settings/base.py
@@ -9,13 +9,14 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__fil
 ADMINS = [('Doruk', 'doruk@gezici.me')]
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.ituscheduler.com', '.elasticbeanstalk.com']
 
-
+EC2_PRIVATE_IP = None
 try:
     EC2_PRIVATE_IP = requests.get('http://169.254.169.254/latest/meta-data/local-ipv4', timeout=0.01).text
-    ALLOWED_HOSTS.append(EC2_PRIVATE_IP)
-except requests.exceptions.RequestException:
+except Exception:
     pass
 
+if EC2_PRIVATE_IP:
+    ALLOWED_HOSTS.append(EC2_PRIVATE_IP)
 
 # Application definition
 
