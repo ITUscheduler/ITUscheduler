7842774479628993c507844e810083f6698af5db
dorukgezici <dorukgezici@gmail.com>
2019-08-27 17:12:20 +0300

    Celery incoming

--- ITUscheduler/settings/celery.py
+++ ITUscheduler/settings/celery.py
@@ -1,3 +1,4 @@
+from __future__ import absolute_import, unicode_literals
 import os
 from celery import Celery
 
--- api/tasks.py
+++ api/tasks.py
@@ -1,3 +1,4 @@
+from __future__ import absolute_import, unicode_literals
 import re
 from django.utils import timezone
 import requests
