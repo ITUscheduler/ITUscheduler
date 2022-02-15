c3abb5f39b0feb0ee1e531e213e838cd21562514
Doruk Gezici <dorukgezici@gmail.com>
2022-02-15 19:39:19 +0300

    Autoscaling task definitions

--- ituscheduler/scheduler/tests.py
+++ ituscheduler/scheduler/tests.py
@@ -1,1 +1,15 @@
-# Create your tests here.
+from django.core import mail
+from django.test import TestCase
+
+
+class EmailTest(TestCase):
+    def test_send_email(self):
+        mail.send_mail(
+            subject='Subject here',
+            message='Here is the message.',
+            from_email='info@ituscheduler.com',
+            recipient_list=['doruk@ituscheduler.com'],
+            fail_silently=False,
+        )
+        self.assertEqual(len(mail.outbox), 1)
+        self.assertEqual(mail.outbox[0].subject, 'Subject here')
